02b20057d145c8ec0d0e8dc3e5fb9b36c5f8ea1a
jocori <jocori@ku.edu>
2024-11-05 19:18:59 -0600

    added further instruction

--- code/analysis.R
+++ code/analysis.R
@@ -134,10 +134,48 @@ confint(m1)
 #plug that NDVI into 1980s relationship from regressions y = mx +b > 
 #solve for elevation at that NDVI value
 
-#split present day and 1980s into seperate dataframes 
-present<-regs_full[regs_full$Year == "Avg1317",]
-past<-regs_full[regs_full$Year == "Avg8488",]
-regs_full$Treeline_elevation
+
+
+
+###########################################
+#set working directory
+setwd("~/Desktop/KU/Projects/GeographicTreelinePatterns")
+#load in packages
+library(dplyr)
+library(lme4)
+library(AICcmodavg)
+# read in data
+regs_full<-read.csv("data/Regressions_28Oct.csv", header = TRUE)
+count(regs_full[regs_full$Significance >= 0.05,])
+
+#Keep linear models only
+regs<-regs_full[regs_full$Regression_Type == "Linear",]
+
+#Keep only significant models
+regs<-regs[regs$Significance<=0.05,]
+
+#Keep only models with negative slope
+regs<-regs[regs$Slope < 0,]
+
+#Keep only average NDVI NOT Max (relevant in year calculation)
+regs <- regs %>% filter(Year %in% c("Avg8488", "Avg9802", "Avg1317"))
+
+#Which mountains are excluded now?
+x1<-unique(regs$Peak)
+x2<-unique(regs_full$Peak)
+# no mountains are completely excluded! 
+
+#remove extraneous columns
+regs<-regs[,!(names(regs)%in%c("Regression_Type","X"))]
+
+# Filter out peak-direction combinations that do not have both years
+regs <- regs %>%
+  group_by(Peak, Direction) %>%
+  filter(all(c("Avg8488", "Avg1317") %in% Year)) %>%
+  ungroup()
+
+present<-regs[regs$Year == "Avg1317",]
+past<-regs[regs$Year == "Avg8488",]
 
 #present day regression equation
 present$present_NDVI<-(present$Treeline_elevation*present$Slope) + present$Intercept
@@ -145,5 +183,104 @@ present$present_NDVI<-(present$Treeline_elevation*present$Slope) + present$Inter
 #plug present day NDVI at treesbegin value into past regression equation to calculate past elevation
 #y = mx + b....x = y-b/m...(present ndvi - intercept)/slope
 past$past_elevation <- (present$present_NDVI -past$Intercept)/past$Slope
-colnames(present$Treeline_elevation) <- "present_elevation" # fix column name
-#get difference
+cols<-names(present)
+colnames(present)[14] <- "present_elevation" # fix column name
+
+#merge past and present
+regs <- merge(present, past, by = intersect(names(present),names(past)), all.y = TRUE, all.x = TRUE)
+#remove unnecessary columns
+regs<-regs[,-c(4:8,15:16)]
+
+# Calculate the change in treeline elevation
+regs <- regs %>%
+  group_by(Peak, Direction) %>%
+  mutate(
+    change_in_treeline_elevation = present_elevation[Year == "Avg1317"] - past_elevation[Year == "Avg8488"]
+  ) %>%
+  ungroup()
+
+#remove present and past elevation columns
+regs<-regs[,-c(9:10)]
+
+#remove year value
+regs<-regs[,-c(3)]
+#remove duplicate rows
+regs <- regs %>%
+  distinct()
+
+#Which mountains are excluded now?
+x1<-unique(regs$Peak)
+x2<-unique(regs_full$Peak)
+setdiff(x2, x1) #"Mt. Harrison"   "Mt. Ovington"   "Mt. Timpanogos" "Star Peak"  
+
+###### mountains excluded from analysis ######
+## "Mt. Harrison"   "Mt. Ovington"   "Mt. Timpanogos" "Star Peak"  
+
+
+## model selection
+hist(regs$change_in_treeline_elevation)
+plot(regs$change_in_treeline_elevation~regs$Lat, data = regs)
+plot(regs$change_in_treeline_elevation~regs$Long, data = regs)
+
+
+m1<-lmer(change_in_treeline_elevation~Lat +Long + (1|Peak_ID), data = regs)
+summary(m1)
+AIC(m1)
+m2<-lmer(change_in_treeline_elevation~Lat *Long + (1|Peak_ID), data = regs)
+summary(m2)
+AIC(m2)
+m3<-lmer(change_in_treeline_elevation~Lat +Long + Stations_After_Treeline +(1|Peak_ID), data = regs)
+summary(m3)
+AIC(m3)
+m4<-lmer(change_in_treeline_elevation~  Stations_After_Treeline +Lat*Long+(1|Peak_ID), data = regs)
+summary(m4)
+AIC(m4)
+m5<-lmer(change_in_treeline_elevation~Lat +Long + Direction +(1|Peak_ID), data = regs)
+summary(m5)
+AIC(m5)
+m6<-lmer(change_in_treeline_elevation~ Direction +Lat*Long+(1|Peak_ID), data = regs)
+summary(m6)
+AIC(m6)
+m7<-lmer(change_in_treeline_elevation~Lat +Long + Stations_After_Treeline +Direction+(1|Peak_ID), data = regs)
+summary(m7)
+AIC(m7)
+m8<-lmer(change_in_treeline_elevation~Stations_After_Treeline +Direction+Lat*Long+(1|Peak_ID), data = regs)
+summary(m8)
+AIC(m8)
+
+mods<-list(m1,m2,m3,m4,m5,m6,m7,m8)
+aictab(cand.set = mods)
+
+##Model 8 is the best model
+m8<-lmer(change_in_treeline_elevation~Stations_After_Treeline +Direction+Lat*Long+(1|Peak_ID), data = regs)
+summary(m8)
+confint(m8)
+
+regs %>%
+  ggplot(aes(y = change_in_treeline_elevation, x = Lat)) + 
+  geom_smooth() + 
+  xlab("Latitude") + 
+  ylab("Change in Treeline Elevation (2017 - 1984)")
+
+regs %>%
+  ggplot(aes(y = change_in_treeline_elevation, x = Long)) + 
+  geom_smooth() + 
+  xlab("Longitude") + 
+  ylab("Change in Treeline Elevation (2017 - 1984)")
+
+regs %>%
+  ggplot(aes(x = Lat, y = change_in_treeline_elevation, color = Long)) + 
+  geom_point(size = 3, alpha = 0.7) + 
+  scale_color_gradient(name = "Longitude", low = "blue", high = "red") + 
+  xlab("Latitude") + 
+  ylab("Change in Treeline Elevation (2017 - 1984)")
+
+
+#should we throw in distance to coast as another predictor??
+
+### genetics and molecular biology 32:203 Diniz-Filho et al 2009...don't necessarily do this, just check it out
+
+
+
+
+
