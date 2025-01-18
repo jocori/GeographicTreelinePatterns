e64637a083f179fc2811c749cc0755d0b939a7b8
jocori <jocori@ku.edu>
2025-01-18 11:13:42 -0600

    commented out pcnm, decided it is unsuitable for data due to clustering within peaks due to different directions

--- code/model_selection.R
+++ code/model_selection.R
@@ -14,7 +14,7 @@ library(spdep)
 library(sp)
 library(spaMM) # spatial mixed models
 library(vegan) # PCNM
-
+library(RSpectra) # for fast calculation of extreme eigenvector in spamm models
 #read in data
 regs<- read.csv("data/Regressions12Nov24.csv")
 #check for correlation among predictor variables
@@ -332,173 +332,177 @@ confint(m2_spamm, parm = names(fixef(m2_spamm))) #significant interaction
 
 
 
-
+#calculate k nearest neighbors for moran's test (test for spatial autocorrelation)
+coords <- as.matrix(cbind(regs$Long, regs$Lat))
+neighbors <- dnearneigh(coords, d1 = 0, d2 = 1500, longlat = TRUE)  # Large threshold for connectivity
+listw <- nb2listw(neighbors, style = "W")
+moran.test(regs$change_in_treeline_elevation, listw = listw)
 
 ## Eigenvector Filtering or PCNM
 # explore Principal Coordinates of Neighbor Matrices (PCNM) 
 # to capture latent spatial patterns
-pcnm_res <- pcnm(dist(coords))
-significant_axes <- pcnm_res$vectors[, 1:5] 
-regs <- cbind(regs, significant_axes)
+#pcnm_res <- pcnm(dist(coords))
+#significant_axes <- pcnm_res$vectors[, 1:5] 
+#regs <- cbind(regs, significant_axes)
 
-m1_pcnm<-lmer(change_in_treeline_elevation~PCNM1 + PCNM2+ (1|Peak_ID),
-              data = regs)
-summary(m1_pcnm)
-AIC(m1_pcnm)
-vif(m1_pcnm)
-moran.test(resid(m1),listw =listw)
+#m1_pcnm<-lmer(change_in_treeline_elevation~PCNM1 + PCNM2+ (1|Peak_ID),
+#              data = regs)
+#summary(m1_pcnm)
+#AIC(m1_pcnm)
+#vif(m1_pcnm)
+#moran.test(resid(m1),listw =listw)
 
-m2_pcnm<-lmer(change_in_treeline_elevation~PCNM1*PCNM2+ (1|Peak_ID), data = regs)
-summary(m2_pcnm)
-AIC(m2_pcnm)
-vif(m2_pcnm)
-moran.test(resid(m2_pcnm),listw =listw)
+#m2_pcnm<-lmer(change_in_treeline_elevation~PCNM1*PCNM2+ (1|Peak_ID), data = regs)
+#summary(m2_pcnm)
+#AIC(m2_pcnm)
+#vif(m2_pcnm)
+#moran.test(resid(m2_pcnm),listw =listw)
 
-m3_pcnm<-lmer(change_in_treeline_elevation~Stations_After_Treeline + PCNM1 + PCNM2+(1|Peak_ID), data = regs)
-summary(m3_pcnm)
-AIC(m3_pcnm)
-vif(m3_pcnm)
-moran.test(resid(m3_pcnm),listw =listw)
+#m3_pcnm<-lmer(change_in_treeline_elevation~Stations_After_Treeline + PCNM1 + PCNM2+(1|Peak_ID), data = regs)
+#summary(m3_pcnm)
+#AIC(m3_pcnm)
+#vif(m3_pcnm)
+#moran.test(resid(m3_pcnm),listw =listw)
 
-m4_pcnm<-lmer(change_in_treeline_elevation~  Stations_After_Treeline + PCNM1*PCNM2+(1|Peak_ID), data = regs)
-summary(m4_pcnm)
-AIC(m4_pcnm)
-vif(m4_pcnm)
-moran.test(resid(m4),listw =listw)
+#m4_pcnm<-lmer(change_in_treeline_elevation~  Stations_After_Treeline + PCNM1*PCNM2+(1|Peak_ID), data = regs)
+#summary(m4_pcnm)
+#AIC(m4_pcnm)
+#vif(m4_pcnm)
+#moran.test(resid(m4_pcnm),listw =listw)
 
-m5_pcnm<-lmer(change_in_treeline_elevation~ PCNM1 + PCNM2+ Direction +(1|Peak_ID), data = regs)
-summary(m5_pcnm)
-AIC(m5_pcnm)
-vif(m5_pcnm)
-moran.test(resid(m5_pcnm),listw =listw)
+#m5_pcnm<-lmer(change_in_treeline_elevation~ PCNM1 + PCNM2+ Direction +(1|Peak_ID), data = regs)
+#summary(m5_pcnm)
+#AIC(m5_pcnm)
+#vif(m5_pcnm)
+#moran.test(resid(m5_pcnm),listw =listw)
 
-m6_pcnm<-lmer(change_in_treeline_elevation~ Direction + PCNM1*PCNM2+(1|Peak_ID), data = regs)
-summary(m6_pcnm)
-AIC(m6_pcnm)
-vif(m6_pcnm)
-moran.test(resid(m6_pcnm),listw =listw)
+#m6_pcnm<-lmer(change_in_treeline_elevation~ Direction + PCNM1*PCNM2+(1|Peak_ID), data = regs)
+#summary(m6_pcnm)
+#AIC(m6_pcnm)
+#vif(m6_pcnm)
+#moran.test(resid(m6_pcnm),listw =listw)
 
-m7_pcnm<-lmer(change_in_treeline_elevation~  PCNM1 + PCNM2 + Stations_After_Treeline +Direction+(1|Peak_ID), data = regs)
-summary(m7_pcnm)
-AIC(m7_pcnm)
-vif(m7_pcnm)
-moran.test(resid(m7_pcnm),listw =listw)
+#m7_pcnm<-lmer(change_in_treeline_elevation~  PCNM1 + PCNM2 + Stations_After_Treeline +Direction+(1|Peak_ID), data = regs)
+#summary(m7_pcnm)
+#AIC(m7_pcnm)
+#vif(m7_pcnm)
+#moran.test(resid(m7_pcnm),listw =listw)
 
-m8_pcnm<-lmer(change_in_treeline_elevation~Stations_After_Treeline +Direction+ PCNM1*PCNM2+(1|Peak_ID), data = regs)
-summary(m8_pcnm)
-AIC(m8_pcnm)
-vif(m8_pcnm)
-moran.test(resid(m8_pcnm),listw =listw)
+#m8_pcnm<-lmer(change_in_treeline_elevation~Stations_After_Treeline +Direction+ PCNM1*PCNM2+(1|Peak_ID), data = regs)
+#summary(m8_pcnm)
+#AIC(m8_pcnm)
+#vif(m8_pcnm)
+#moran.test(resid(m8_pcnm),listw =listw)
 
-m9_pcnm<-lmer(change_in_treeline_elevation~PCNM1 + PCNM2+dist_coast+(1|Peak_ID), data = regs)
-summary(m9_pcnm)
-AIC(m9_pcnm)
-vif(m9_pcnm)
-moran.test(resid(m9_pcnm),listw =listw)
+#m9_pcnm<-lmer(change_in_treeline_elevation~PCNM1 + PCNM2+dist_coast+(1|Peak_ID), data = regs)
+#summary(m9_pcnm)
+#AIC(m9_pcnm)
+#vif(m9_pcnm)
+#moran.test(resid(m9_pcnm),listw =listw)
 
-m10_pcnm<-lmer(change_in_treeline_elevation~dist_coast+ PCNM1*PCNM2 +(1|Peak_ID), data = regs)
-summary(m10_pcnm)
-AIC(m10_pcnm)
-vif(m10_pcnm)
-moran.test(resid(m10_pcnm),listw =listw)
+#m10_pcnm<-lmer(change_in_treeline_elevation~dist_coast+ PCNM1*PCNM2 +(1|Peak_ID), data = regs)
+#summary(m10_pcnm)
+#AIC(m10_pcnm)
+#vif(m10_pcnm)
+#moran.test(resid(m10_pcnm),listw =listw)
 
-m11_pcnm<-lmer(change_in_treeline_elevation~dist_coast+ PCNM1 + PCNM2+Stations_After_Treeline+(1|Peak_ID), data = regs)
-summary(m11_pcnm)
-AIC(m11_pcnm)
-vif(m11_pcnm)
-moran.test(resid(m11_pcnm),listw =listw)
+#m11_pcnm<-lmer(change_in_treeline_elevation~dist_coast+ PCNM1 + PCNM2+Stations_After_Treeline+(1|Peak_ID), data = regs)
+#summary(m11_pcnm)
+#AIC(m11_pcnm)
+#vif(m11_pcnm)
+#moran.test(resid(m11_pcnm),listw =listw)
 
-m12_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Stations_After_Treeline+ PCNM1*PCNM2+(1|Peak_ID), data = regs)
-summary(m12_pcnm)
-AIC(m12_pcnm)
-vif(m12_pcnm)
-moran.test(resid(m12_pcnm),listw =listw)
+#m12_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Stations_After_Treeline+ PCNM1*PCNM2+(1|Peak_ID), data = regs)
+#summary(m12_pcnm)
+#AIC(m12_pcnm)
+#vif(m12_pcnm)
+#moran.test(resid(m12_pcnm),listw =listw)
 
-m13_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+ PCNM1 + PCNM2+ +(1|Peak_ID), data = regs)
-summary(m13_pcnm)
-AIC(m13_pcnm)
-vif(m13_pcnm)
-moran.test(resid(m13_pcnm),listw =listw)
+#m13_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+ PCNM1 + PCNM2+ +(1|Peak_ID), data = regs)
+#summary(m13_pcnm)
+#AIC(m13_pcnm)
+#vif(m13_pcnm)
+#moran.test(resid(m13_pcnm),listw =listw)
 
-m14_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+ PCNM1*PCNM2 +(1|Peak_ID), data = regs)
-summary(m14_pcnm)
-AIC(m14_pcnm)
-vif(m14_pcnm)
-moran.test(resid(m14_pcnm),listw =listw)
+#m14_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+ PCNM1*PCNM2 +(1|Peak_ID), data = regs)
+#summary(m14_pcnm)
+#AIC(m14_pcnm)
+#vif(m14_pcnm)
+#moran.test(resid(m14_pcnm),listw =listw)
 
-m15_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+ PCNM1 + PCNM2+Stations_After_Treeline+(1|Peak_ID), data = regs)
-summary(m15_pcnm)
-AIC(m15_pcnm)
-vif(m15_pcnm)
-moran.test(resid(m15_pcnm),listw =listw)
+#m15_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+ PCNM1 + PCNM2+Stations_After_Treeline+(1|Peak_ID), data = regs)
+#summary(m15_pcnm)
+#AIC(m15_pcnm)
+#vif(m15_pcnm)
+#moran.test(resid(m15_pcnm),listw =listw)
 
-m16_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+Stations_After_Treeline+ PCNM1*PCNM2 +(1|Peak_ID), data = regs)
-summary(m16_pcnm)
-AIC(m16_pcnm)
-vif(m16_pcnm)
-moran.test(resid(m16_pcnm),listw =listw)
+#m16_pcnm<-lmer(change_in_treeline_elevation~dist_coast+Direction+Stations_After_Treeline+ PCNM1*PCNM2 +(1|Peak_ID), data = regs)
+#summary(m16_pcnm)
+#AIC(m16_pcnm)
+#vif(m16_pcnm)
+#moran.test(resid(m16_pcnm),listw =listw)
 
 #best PCNM model
-mods_pcnm<-list(m1_pcnm,m2_pcnm,m3_pcnm,m4_pcnm,m5_pcnm,
-           m6_pcnm,m7_pcnm,m8_pcnm,m9_pcnm,m10_pcnm,
-           m11_pcnm,m12_pcnm,m13_pcnm,m14_pcnm,m15_pcnm,m16_pcnm)
+#mods_pcnm<-list(m1_pcnm,m2_pcnm,m3_pcnm,m4_pcnm,m5_pcnm,
+#           m6_pcnm,m7_pcnm,m8_pcnm,m9_pcnm,m10_pcnm,
+#           m11_pcnm,m12_pcnm,m13_pcnm,m14_pcnm,m15_pcnm,m16_pcnm)
 
 #aictab(cand.set = mods_pcnm)
 #model 8 is best
 #confint(m8_pcnm) #only PCNM 1 is significant
 # Define terms for pcnm models
-pcnm_terms <- c(
-  "PCNM1 + PCNM2",
-  "PCNM1 * PCNM2",
-  "Stations_After_Treeline + PCNM1 + PCNM2",
-  "Stations_After_Treeline + PCNM1 * PCNM2",
-  "PCNM1 + PCNM2 + Direction",
-  "Direction + PCNM1 * PCNM2",
-  "PCNM1 + PCNM2 + Stations_After_Treeline + Direction",
-  "Stations_After_Treeline + Direction + PCNM1 * PCNM2",
-  "PCNM1 + PCNM2 + dist_coast",
-  "dist_coast + PCNM1 * PCNM2",
-  "dist_coast + PCNM1 + PCNM2 + Stations_After_Treeline",
-  "dist_coast + Stations_After_Treeline + PCNM1 * PCNM2",
-  "dist_coast + Direction + PCNM1 + PCNM2",
-  "dist_coast + Direction + PCNM1 * PCNM2",
-  "dist_coast + Direction + Stations_After_Treeline + PCNM1 + PCNM2",
-  "dist_coast + Direction + Stations_After_Treeline + PCNM1 * PCNM2"
-)
+#pcnm_terms <- c(
+#  "PCNM1 + PCNM2",
+#  "PCNM1 * PCNM2",
+#  "Stations_After_Treeline + PCNM1 + PCNM2",
+#  "Stations_After_Treeline + PCNM1 * PCNM2",
+#  "PCNM1 + PCNM2 + Direction",
+#  "Direction + PCNM1 * PCNM2",
+#  "PCNM1 + PCNM2 + Stations_After_Treeline + Direction",
+#  "Stations_After_Treeline + Direction + PCNM1 * PCNM2",
+#  "PCNM1 + PCNM2 + dist_coast",
+#  "dist_coast + PCNM1 * PCNM2",
+#  "dist_coast + PCNM1 + PCNM2 + Stations_After_Treeline",
+#  "dist_coast + Stations_After_Treeline + PCNM1 * PCNM2",
+#  "dist_coast + Direction + PCNM1 + PCNM2",
+#  "dist_coast + Direction + PCNM1 * PCNM2",
+#  "dist_coast + Direction + Stations_After_Treeline + PCNM1 + PCNM2",
+#  "dist_coast + Direction + Stations_After_Treeline + PCNM1 * PCNM2"
+#)
 # Extract AIC values from the aic_values matrix
-aic_values_pcnm <- sapply(mods_pcnm, extractAIC)
-aic_values_pcnm <-aic_values_pcnm[2,]
-delta_AIC_pcnm <-aic_values_pcnm - min(aic_values_pcnm) # calculate delta AIC
-AIC_weight_pcnm <-exp(-0.5 * delta_AIC_pcnm) / 
-  sum(exp(-0.5 * delta_AIC_pcnm)) #calculate AIC weights
+#aic_values_pcnm <- sapply(mods_pcnm, extractAIC)
+#aic_values_pcnm <-aic_values_pcnm[2,]
+#delta_AIC_pcnm <-aic_values_pcnm - min(aic_values_pcnm) # calculate delta AIC
+#AIC_weight_pcnm <-exp(-0.5 * delta_AIC_pcnm) / 
+#  sum(exp(-0.5 * delta_AIC_pcnm)) #calculate AIC weights
 # Create a data frame
-model_names <- paste0("m", 1:16, "_pcnm")
-comparison_table_pcnm <- data.frame(
-  Model = model_names,
-  Terms = pcnm_terms,
-  AIC = format_numeric(aic_values_pcnm),
-  Delta_AIC = format_numeric(delta_AIC_pcnm),
-  Weight = format_numeric(AIC_weight_pcnm)
-)
+#model_names <- paste0("m", 1:16, "_pcnm")
+#comparison_table_pcnm <- data.frame(
+#  Model = model_names,
+#  Terms = pcnm_terms,
+# AIC = format_numeric(aic_values_pcnm),
+# Delta_AIC = format_numeric(delta_AIC_pcnm),
+# Weight = format_numeric(AIC_weight_pcnm)
+#)
 
 
 # Sort by AIC
-comparison_table_pcnm <- comparison_table_pcnm[order(as.numeric(comparison_table_pcnm$AIC)), ]
+#comparison_table_pcnm <- comparison_table_pcnm[order(as.numeric(comparison_table_pcnm$AIC)), ]
 
 # Print the resulting table
-print(comparison_table_pcnm)
+#print(comparison_table_pcnm)
 
 # Save to CSV
-write.csv(
-  comparison_table_pcnm,
-  file = "results/pcnm_AIC.csv",
-  row.names = FALSE
-)
+#write.csv(
+#  comparison_table_pcnm,
+#  file = "results/pcnm_AIC.csv",
+#  row.names = FALSE
+#)
 
 
 #####################################
 #######best model overall out of linear mixed models, spatial mixed models, and pcnm
-all_mods <- c(mods, mods_spamm, mods_pcnm)
+all_mods <- c(mods, mods_spamm)
 # Extract AIC for each model
 aic_values <- sapply(all_mods, extractAIC)
 
@@ -512,15 +516,15 @@ model_names_group1 <- paste0("m", 1:16)
 # Generate model names for the second group
 model_names_group2 <- paste0("m", 1:16, "_spamm")
 # Generate model names for the third group
-model_names_group3 <- paste0("m", 1:16, "_pcnm")
+#model_names_group3 <- paste0("m", 1:16, "_pcnm")
 # Combine all the model names into one vector
-model_names <- c(model_names_group1, model_names_group2, model_names_group3)
+model_names <- c(model_names_group1, model_names_group2)
 
 
 delta_AIC <-aic_values_only - min(aic_values_only) # calculate delta AIC
 AIC_weight <-exp(-0.5 * delta_AIC) / 
   sum(exp(-0.5 * delta_AIC)) #calculate AIC weights
-terms<- c(lmer_terms, spamm_terms, pcnm_terms)
+terms<- c(lmer_terms, spamm_terms)
 # Create a data frame
 
 comparison_table<- data.frame(
@@ -547,8 +551,8 @@ write.csv(
 )
 
 ######best model out of top models in each category
-mods_best<-list(m8, m2_spamm, m9_pcnm)
-model_names <- c("m8","m2_spamm","m8_pcnm")
+mods_best<-list(m8, m2_spamm)
+model_names <- c("m8","m2_spamm")
 # Extract AIC for each model
 aic_values <- sapply(mods_best, extractAIC)
 
@@ -560,8 +564,8 @@ AIC_weight <-exp(-0.5 * delta_AIC) /
   sum(exp(-0.5 * delta_AIC)) #calculate AIC weights
 m8_terms <- c("Stations_After_Treeline + Direction + Lat * Long")
 m2_spamm_terms <- c("Lat * Long")
-m9_pcnm_terms <- c("PCNM1 + PCNM2 + dist_coast")
-terms<- c(m8_terms,m2_spamm_terms,m9_pcnm_terms) # terms for table calrity
+#m9_pcnm_terms <- c("PCNM1 + PCNM2 + dist_coast")
+terms<- c(m8_terms,m2_spamm_terms) # terms for table calrity
 comparison_table <- data.frame(
   Model = model_names,
   Terms = terms,
@@ -624,7 +628,7 @@ extract_model_output <- function(model, model_name) {
 
 # Extract and save outputs for the three best models
 m8_output <- extract_model_output(m8, "m8")
-m9_pcnm_output <- extract_model_output(m9_pcnm, "m9_pcnm")
+#m9_pcnm_output <- extract_model_output(m9_pcnm, "m9_pcnm")
 
 ## USING THIS SPAMM MODEL TO REPORT OUTPUT, MAXIMIZING P_V IN CI CALCULATION, A LIKLIHOOD ESTIMATE
 #refit model with suggested parameters
@@ -639,3 +643,7 @@ refitted_model <- update(m2_spamm, init = best_fit_params)
 summary(refitted_model)
 confint(refitted_model, parm = names(fixed_effects))
 ###HAD TO MANUALLY TYPE TABLE FOR THE SPAMM MODEL
+
+#for manually extracting random effects and adding them to the table
+summary(m8)
+#summary(m9_pcnm)
