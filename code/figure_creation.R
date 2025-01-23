2c041713a74240f839537ead4afacef0bca32f9f
jocori <jocori@ku.edu>
2025-01-23 14:38:24 -0600

    coded figure 5 and figure 6....not going to end up using figure 6

--- code/figure_creation.R
+++ code/figure_creation.R
@@ -1,6 +1,6 @@
 #set working directory
 setwd("~/Desktop/KU/Projects/GeographicTreelinePatterns")
-regs<- read.csv("data/Regressions12Nov24.csv")
+regs<- read.csv("data/regs_final22jan25.csv")
 
 #load packages
 library(dplyr)
@@ -28,15 +28,21 @@ format_long <- function(long) {
   ifelse(long >= 0, paste0(long, "° E"), paste0(abs(long), "° W"))
 }
 
+#define panel labels based on journal specifications
+tags<- c("(a)","(b)")
 
 # create latitude plot
-png("figures/figure5.png", width = 7.5, height = 5, units = "in", res = 300)
+png("figures/figure4.png", width = 7.5, height = 5, units = "in", res = 300)
 lat_plot <- regs %>%
   ggplot(aes(x = Lat, y = change_in_treeline_elevation)) +
   geom_point(colour = "#a845b9") +
   geom_abline(slope = -14.63, intercept = 611.69, colour = "black", lwd = 0.75) +
   labs(x = "Latitude", y = "Change in Treeline Elevation") +
-  scale_x_continuous(labels = format_lat) +  # Apply latitude formatting
+  scale_x_continuous(limits = c(10, 50),  # Ensure the entire latitude range is covered
+                     breaks = seq(10, 50, by = 10),
+                     labels = format_lat) +  # Apply latitude formatting
+  annotate("text", x = 10, y = max(regs$change_in_treeline_elevation),
+           label = "(a)", hjust = -0.1, vjust = 0.2, size = 5) +
   theme_minimal() +
   theme(
     panel.grid = element_blank(),
@@ -54,6 +60,8 @@ long_plot <- regs %>%
   geom_abline(slope = 14.313, intercept = 1636.478, colour = "black", lwd = 0.75) +
   labs(x = "Longitude", y = element_blank()) +
   scale_x_continuous(labels = format_long) +  # Apply longitude formatting
+  annotate("text", x = min(regs$Long), y = max(regs$change_in_treeline_elevation),
+           label = "(b)", hjust = -0.1, vjust = 0.2, size = 5) +
   theme_minimal() +
   theme(
     panel.grid = element_blank(),
@@ -65,7 +73,7 @@ long_plot <- regs %>%
   )
 
 # Combine the plots
-lat_plot + long_plot  # Combines the two plots side by side
+lat_plot + long_plot # Combines the two plots side by side
 dev.off()
 
 #distance to coast vs longitude
@@ -120,3 +128,111 @@ dev.off()
 #chir<-dat[dat$Name == "Cerro Chirippo",]
 #chir<-chir[,c(3,11,14:16)]
 #write.csv(chir,"data/chirippo.csv")
+
+###figure 5
+#get the slope for lines on the graph
+m1<-lm(regs$change_in_treeline_NDVI~regs$Lat)
+summary(m1)
+m2<-lm(regs$change_in_treeline_NDVI~regs$Long)
+summary(m2)
+#########################
+library(ggplot2)
+library(patchwork)
+
+#Figure 5
+# Define a function to format coordinates with degree symbols
+format_lat <- function(lat) {
+  ifelse(lat >= 0, paste0(lat, "° N"), paste0(abs(lat), "° S"))
+}
+
+format_long <- function(long) {
+  ifelse(long >= 0, paste0(long, "° E"), paste0(abs(long), "° W"))
+}
+
+#define panel labels based on journal specifications
+tags<- c("(a)","(b)")
+
+# create latitude plot
+png("figures/figure5.png", width = 7.5, height = 5, units = "in", res = 300)
+regs %>%
+  ggplot(aes(x = Lat, y = change_in_treeline_NDVI)) +
+  geom_point(colour = "#a845b9") +
+  geom_abline(slope = -0.0035301, intercept = 0.1308954, colour = "black", lwd = 0.75) +
+  labs(x = "Latitude", y = "Change in Treeline NDVI") +
+  scale_x_continuous(limits = c(10, 50),  # Ensure the entire latitude range is covered
+                     breaks = seq(10, 50, by = 10),
+                     labels = format_lat) +  # Apply latitude formatting
+  annotate("text", x = 10, y = max(regs$change_in_treeline_NDVI),
+           label = "(a)", hjust = -0.1, vjust = 0.2, size = 5) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    axis.line = element_line(colour = "black", linewidth = 0.25),
+    axis.text = element_text(family = "sans"),
+    axis.ticks = element_line(),
+    plot.title = element_blank(),
+    axis.title = element_text(size = 13, family = "sans")
+  )
+
+# Create the Longitude plot
+#long_plot <- regs %>%
+#  ggplot(aes(x = Long, y = change_in_treeline_NDVI)) +
+#  geom_point(colour = "#5b6c65") +
+#  geom_abline(slope = 0.0039667, intercept = 0.4376782, colour = "black", lwd = 0.75) +
+#  labs(x = "Longitude", y = element_blank()) +
+#  scale_x_continuous(labels = format_long) +  # Apply longitude formatting
+#  annotate("text", x = min(regs$Long), y = max(regs$change_in_treeline_NDVI),
+#           label = "(b)", hjust = -0.1, vjust = 0.2, size = 5) +
+#  theme_minimal() +
+#  theme(
+#    panel.grid = element_blank(),
+#    axis.line = element_line(colour = "black", linewidth = 0.25),
+#    axis.text = element_text(family = "sans"),
+#    axis.ticks = element_line(),
+#    plot.title = element_blank(),
+#    axis.title = element_text(size = 13, family = "sans")
+#  )
+
+# Combine the plots
+#lat_plot + long_plot # Combines the two plots side by side
+dev.off()
+
+##figure 6.....didn't end up using these but scared to delete the code just in case
+#read in full dataset 
+regs_fig6<-read.csv("data/full_regs_for_figure6.csv")
+#fix dataframe
+regs_fig6<-regs_fig6 %>%
+  mutate(
+    treeline_elevation = case_when(
+      Year == "Avg1317" ~ present_elevation,      # Keep values from column1 for year 2017
+      Year == "Avg8488" ~ past_elevation,      # Use values from column2 for year 1984
+      TRUE ~ NA_real_              # Handle any other cases (if applicable)
+    )
+  )
+
+regs_fig6 %>%
+  ggplot(aes(x = Year, y = treeline_elevation)) +
+  geom_boxplot()+
+  labs(x = "Year", y = "Treeline Elevation (m)") +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    axis.line = element_line(colour = "black", linewidth = 0.25),
+    axis.text = element_text(family = "sans"),
+    axis.ticks = element_line(),
+    plot.title = element_blank(),
+    axis.title = element_text(size = 13, family = "sans")
+  )
+regs_fig6 %>%
+  ggplot(aes(x = Year, y = treeline_ndvi)) +
+  geom_boxplot()+
+  labs(x = "Year", y = "NDVI at 2017 Treeline Elevation") +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    axis.line = element_line(colour = "black", linewidth = 0.25),
+    axis.text = element_text(family = "sans"),
+    axis.ticks = element_line(),
+    plot.title = element_blank(),
+    axis.title = element_text(size = 13, family = "sans")
+  )
