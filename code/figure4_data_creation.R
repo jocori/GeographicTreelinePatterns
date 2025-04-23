d534891acecd9f7046bd4e264a49c32e5c8d67b6
jocori <jocori@ku.edu>
2025-04-23 16:11:01 -0500

    cleaned up code, switched to median instead of mean for slope summary

--- code/figure4_data_creation.R
+++ code/figure4_data_creation.R
@@ -5,35 +5,26 @@ locs<-unique(loc[,"Peak"])
 library(dplyr)
 
 # Use dplyr to get one latitude and longitude for each unique Peak
+#for all unqiue peaks, 
+#get median slope, averaage latitude, and mean longitude for each mountain, 
+#then put these summaries as new columns in unique peaks 
 unique_peaks <- reg %>%
+  filter(Peak != "Mt. Washington") %>% #Remove mt. washington as it's too spatially different from others
   group_by(Peak) %>%
-  summarize(Lat = first(Lat), Long = first(Long), Slope = first(Slope))
+  summarize(Lat = mean(Lat), Long = mean(Long), Slope = median(Slope))
 
 # View the result
 print(unique_peaks)
-#Remove mt. washington as it's too spatially different from others
-unique_peaks<-unique_peaks[unique_peaks$Peak != "Mt. Washington",]
-#for all unqiue peaks, 
-#get average slope, latitude, and longitude for each mountain, 
-#then put these averages as new columns in unique peaks 
-# compute the averages from the full `reg` data
-avg_stats <- reg %>%
-  filter(Peak != "Mt. Washington") %>%
-  group_by(Peak) %>%
-  summarise(
-    slope     = mean(Slope,     na.rm = TRUE),
-    latitude  = mean(Lat,  na.rm = TRUE),
-    longitude = mean(Long, na.rm = TRUE)
-  )
 
-# join them back onto your unique_peaks
-unique_peaks <- unique_peaks %>%
-  left_join(avg_stats, by = "Peak")
+
+
+
+
 # add column for sign of slope
 #if positive -> 1
 #if negative -> 0
 
-unique_peaks$sign <- ifelse(unique_peaks$Slope > 0, 1,0)
-
-
+unique_peaks$sign <- ifelse(unique_peaks$slope > 0, 1,0)
+mean(reg$Slope)
+mean(unique_peaks$Slope)
 write.csv(unique_peaks, "data/figure4_peaks.csv")
